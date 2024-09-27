ff8d731e4c887c65462e51c8948d75c6793ecabf
Оксана <l_oksana@list.ru>
2024-09-27 17:43:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,56 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let ArrayFromExpr = [];
+    while (expr.length) {
+    let substring = expr.substring(0, 10);
+    expr = expr.substring(10);
+      
+    if (substring === '**********') {
+      ArrayFromExpr.push(' ');      
+    } else {
+      ArrayFromExpr.push(substring);          
+    }
+  }  
+  
+  const getChangeArray = (array) => {
+    return array.map(item => item.match(/..?/g));    
+  }
+  let changeArray = getChangeArray(ArrayFromExpr);
+ 
+
+  const getFilterArray = (array) => {
+    return array.map(item => item.filter(item => item !== '00'));
+  }; 
+  let filterArray = getFilterArray(changeArray);
+  
+
+  let dictionary = {
+    '10': '.',
+    '11': '-',
+  };
+
+  const getMorseMeaning = (array) => {
+    return array.map(item => item.map(item => dictionary[item] || item));
+  }
+  let morseCode = getMorseMeaning(filterArray);
+  
+  
+  const getExprOfMorseCode = (array) => {
+    return array.map(item => item.join(''));
+  }
+  let morseCodeExpr = getExprOfMorseCode(morseCode);
+   
+  
+  const getExprOfMorseTable = (array) => {
+    return array.map(item => MORSE_TABLE[item] || item);
+  }
+  let result = getExprOfMorseTable(morseCodeExpr);
+    
+  return result.join('');
 }
+
+decode("0000101110000011111100101110100010111010000000101000000011100000111110**********00001010100011101110000011111100101111100000000010**********000010101000111011100010101010000011111100001111110010111010");
 
 module.exports = {
     decode
